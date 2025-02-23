67aad38eeac67c7195eff4dc091a654516e8ca5d
tarzaniskingg <121493718+tarzaniskingg@users.noreply.github.com>
2025-02-23 21:24:45 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -8,27 +8,36 @@ import plotly.graph_objects as go
 st.set_page_config(layout="wide")
 st.title('AbbVie Inc. Capital Asset Pricing Model and Beta')
 
+indexmap = {0: "SPX", 1:"NASDAQ", 2:"DJI"}
+indexselection = st.pills("Index", options=indexmap.keys(), format_func=lambda x: indexmap[x], selection_mode="single", default= 0)
 timemap = {0: "Daily", 1:"Weekly", 2:"Monthly"}
-timeselection = st.pills("Frequency", options=timemap.keys(), format_func=lambda x: timemap[x], selection_mode="single")
+timeselection = st.pills("Frequency", options=timemap.keys(), format_func=lambda x: timemap[x], selection_mode="single", default= 1)
 if timeselection == 0:
     rangemap = {0: "6 months", 1: "1 year", 2: "2 years", 3: "5 years"}
+    weekly = pd.read_csv('rawdaily.csv')
 elif timeselection == 1:
     rangemap = {0: "6 months", 1:"1 year", 2: "2 years"}
+    weekly = pd.read_csv('rawweekly.csv')
 elif timeselection == 2:
     rangemap = {0: "6 months", 1: "1 year"}
+    weelkly = pd.read_csv('rawmonthly.csv')
 else:
     rangemap = {0: "6 months", 1: "1 year", 2: "2 years", 3: "5 years"}
-rangeselection = st.pills("Range", options=rangemap.keys(), format_func=lambda x: rangemap[x], selection_mode="single")
-weekly = pd.read_csv('rawweekly.csv')
+rangeselection = st.pills("Regression Range", options=rangemap.keys(), format_func=lambda x: rangemap[x], selection_mode="single", default=1)
 weekly['Dates'] = pd.to_datetime(weekly['Dates'])
 weekly.set_index('Dates', inplace=True)
 weekly.sort_index(inplace=True)
 endog = weekly['ABBV']
-exog = sm.add_constant(weekly['SPX'])
-weeklywindow = {0: 52, 1: 104, 2:260}
+exog = sm.add_constant(weekly[indexmap[indexselection]])
+if timeselection == 0:
+    weeklywindow = {0: 126, 1: 252, 2: 252*2, 3:252*5}
+elif timeselection == 1:
+    weeklywindow = {0: 26, 1: 52, 2: 104}
+elif timeselection == 2:
+    weeklywindow = {0: 180, 1: 365}
 model = RollingOLS(endog, exog, window=weeklywindow[rangeselection]).fit()
 
-weekly['Rolling Beta'] = model.params['SPX']
+weekly['Rolling Beta'] = model.params[indexmap[indexselection]]
 weekly_beta = weekly.dropna()
 fig = go.Figure()
 
@@ -67,9 +76,55 @@ time_horizons = {
     '3Y': 156, # ~156 weeks forward
     '5Y': 260  # ~260 weeks forward
 }
+irr_horizons = {
+    '3M': 1/4,  # ~13 weeks forward
+    '6M': 1/2,  # ~26 weeks forward
+    '1Y': 1,  # ~52 weeks forward
+    '2Y': 2,  # ~104 weeks forward
+    '3Y': 3,  # ~156 weeks forward
+    '5Y': 5  # ~260 weeks forward
+
+}
 weekly['ER'] = weekly['Rolling Beta'] * mrp/100 + weekly['10Y Yield']/100
+weekly['ER'] = ((weekly['ER'] + 1) ** irr_horizons[timehorizon]) - 1
+weekly['10Y Yield'] = ((weekly['10Y Yield']/100 + 1) ** irr_horizons[timehorizon]) - 1
+
 weekly['RR'] = weekly['ABBVP'].pct_change(periods=time_horizons[timehorizon]).shift(-time_horizons[timehorizon])
+
 weekly_sliced = weekly.dropna()
+fig3 = go.Figure()
+
+fig3.add_trace(go.Scatter(
+    x=weekly_sliced.index,
+    y=weekly_sliced['ER'],
+    mode='lines',
+    name='Expected Return'
+))
+
+fig3.add_trace(go.Scatter(
+    x=weekly_sliced.index,
+    y=weekly_sliced['10Y Yield'],
+    mode='lines',
+    name='Risk-free Rate'
+))
+
+# Lock the zoom and disable panning
+fig3.update_layout(
+    title="Expected Return Over Time",
+    xaxis=dict(fixedrange=False),  # Lock zoom for x-axis
+    yaxis=dict(tickformat = "1%", fixedrange=True),  # Lock zoom for y-axis
+    dragmode=False,  # Prevent zooming and panning
+    legend = dict(
+        x=0.90,  # Position legend inside the chart (left side)
+        y=0.98,  # Near the top
+        bgcolor="rgba(255,255,255,0.6)",  # Semi-transparent white background
+        bordercolor="black",
+        borderwidth=1
+    )
+)
+
+# Display locked chart in Streamlit
+st.plotly_chart(fig3, use_container_width=True, key = fig3)
 
 fig2 = go.Figure()
 
@@ -87,12 +142,12 @@ fig2.add_trace(go.Scatter(
 ))
 # Lock the zoom and disable panning
 fig2.update_layout(
-    title="Expected Return vs Annualised Return Over Time",
+    title="Expected Return vs Realised Return Over Time",
     xaxis=dict(fixedrange=False),  # Lock zoom for x-axis
     yaxis=dict(tickformat = "1%", fixedrange=True),  # Lock zoom for y-axis
     dragmode=False,  # Prevent zooming and panning
     legend = dict(
-        x=0.75,  # Position legend inside the chart (left side)
+        x=0.90,  # Position legend inside the chart (left side)
         y=0.98,  # Near the top
         bgcolor="rgba(255,255,255,0.6)",  # Semi-transparent white background
         bordercolor="black",
